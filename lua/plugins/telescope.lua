a3c8eb5df8eec15f8f2ff3dbb48edfa326391ecc
Sq4e <gTTgFrank@foxmail.com>
2026-08-30 14:28:20 +0800

    optimize lazy load

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,8 @@
 return {
   'nvim-telescope/telescope.nvim',
   branch = '0.1.x',
+  -- 懒加载：首次使用 Telescope 命令/LSP 跳转时才加载（节省启动 ~10ms）
+  cmd = 'Telescope',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
     require('telescope').setup({
