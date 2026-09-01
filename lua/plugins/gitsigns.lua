89816e03f5d58a6910b633feee677bfc7e4afbe0
Sq4e <gTTgFrank@foxmail.com>
2026-09-01 22:53:08 +0800

    git sign

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,24 @@
+return {
+    "lewis6991/gitsigns.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+        -- sign style
+        signs = {
+            add = { text = "▎" },
+            change = { text = "▎" },
+            delete = { text = "▎" },
+            topdelete = { text = "▔" },
+            changedelete = { text = "▎" },
+            untracked = { text = "▎" },
+        },
+    },
+    keys = {
+        { "]c", function() require("gitsigns").next_hunk() end, desc = "Git: Next hunk" },
+        { "[c", function() require("gitsigns").prev_hunk() end, desc = "Git: Prev hunk" },
+        { "<leader>hs", function() require("gitsigns").stage_hunk() end, desc = "Git: Stage hunk" },
+        { "<leader>hr", function() require("gitsigns").reset_hunk() end, desc = "Git: Reset hunk" },
+        { "<leader>hp", function() require("gitsigns").preview_hunk() end, desc = "Git: Preview hunk" },
+        { "<leader>hd", function() require("gitsigns").diffthis() end, desc = "Git: Review buffer diff" },
+        { "<leader>hb", function() require("gitsigns").blame_line() end, desc = "Git: Blame line" },
+    },
+}
